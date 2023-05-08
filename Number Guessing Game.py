85a5c04a2e972bd69101272db84a0dab889aa93f
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2023-05-08 20:36:00 +0100

    Update Number Guessing Game.py
    Added the number of attempts it took the player

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,18 +1,20 @@
 # Number Guessing Game
 import random
 
-print('Lets Guess the Number!')
-print('It will be a number between 1 and 30')
+print('Let\'s Play: Guess the Number!')
+print('The number will be between 1 and 30')
 
 def guessNumber():
     number = random.randint(1,30)
     guess = 0
+    attempts = 0
     while guess != number:
         guess = int(input('What is your guess? '))
+        attempts += 1
         if guess < number:
             print("Too low")
         elif guess > number:
             print("Too high")
-    print('Congratulations! You guessed the random number: {}'.format(number))
+    print('Congratulations! You guessed the random number: {}! It took you {} guesses!'.format(number, attempts))
 
 guessNumber()
