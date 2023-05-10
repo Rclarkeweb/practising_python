822d50c29d358420a8415e1e0307a6518f5aa88c
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2023-05-10 11:59:31 +0100

    Update Scrabble.py
    Added ask user for input and work out their word score.

--- Scrabble.py
+++ Scrabble.py
@@ -1,28 +1,30 @@
+# Find out a words Scrabble score
+
+# Create a list of letters and a list of numbers
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+# Combine the two lists into a dictionary
 letter_to_points = {key:value for key, value in zip(letters, points)}
 
+# Set letter to points as empty
 letter_to_points[" "] = 0
 
+# Create function
 def score_word(word):
   point_total = 0
   for letter in word:
     point_total += letter_to_points.get(letter, 0)
   return point_total
 
-brownie_points = score_word("BROWNIE")
-print(brownie_points)
+# Ask user for word
+input_word = input("Enter a word: ").upper()
 
-player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"],"wordNerd": ["EARTH", "EYES", "MACHINE"], "LexiCon": ["ERASER", "BELLY", "HUSKY", "ZAP"], "ProfReader": ["ZAP", "COMA", "PERIOD"]
-}
+# Calculate users score
+user_word = score_word(input_word)
 
-player_to_points = {}
+# Print out answer to user
+print("{} is worth {} points!".format(input_word, user_word))
 
-for player, words in player_to_words.items():
-  player_points = 0
-  for word in words:
-    player_points += score_word(word)
-  player_to_points[player] = player_points
-
-print(player_to_points)
+if user_word > 10:
+    print("That's a good score!")
