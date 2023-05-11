462cfb391d2536076b6e13f1b9ef763a109b5dac
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2023-05-11 17:55:20 +0100

    Create Odd-or-Even.py

---
+++ Odd-or-Even.py
@@ -0,0 +1,11 @@
+# Odd or Even
+
+print('Lets check if a number is Odd or Even')
+
+user_num = int(input('Enter a number: '))
+
+if (user_num % 2) == 0:
+    print("Your number is EVEN")
+else:
+    print("Your number is ODD")
+
