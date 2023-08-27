eb2765a26b7bedd8fe5e1caea378544f7c27265b
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2023-08-27 15:15:28 +0100

    Update Egg boxes.py

--- Egg boxes.py
+++ Egg boxes.py
@@ -1,11 +1,11 @@
 # Egg Boxes. How many omlettes can you make?
 
-numofboxes = int(input("How many boxes of eggs do you have?: "))
+num_of_boxes = int(input("How many boxes of eggs do you have?: "))
 eggs_in_box = 6
 eggs_for_omelette = 4
-total = numofboxes * eggs_in_box // eggs_for_omelette
-leftover = numofboxes * eggs_in_box % eggs_for_omelette
-msg = "You can make {} omelettes with {} boxes of eggs".format(total,numofboxes)
-leftmsg = "You would have {} eggs remaining".format(leftover)
+total = num_of_boxes * eggs_in_box // eggs_for_omelette
+leftover = num_of_boxes * eggs_in_box % eggs_for_omelette
+msg = "You can make {} omelettes with {} boxes of eggs".format(total,num_of_boxes)
+left_msg = "You would have {} eggs remaining".format(leftover)
 print(msg)
-print(leftmsg)
+print(left_msg)
